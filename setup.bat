7779bf859aa3c62dd0cdd8758ac020a3b5d31df0
JTriggerFish <FastTriggerFish@gmail.com>
2023-09-15 14:39:53 +0100

    Build system cleanup in progress

--- setup.bat
+++
@@ -1,11 +0,0 @@
-
-@echo off
-python -m venv venv
-.env\Scriptsctivate
-python -m pip install --upgrade pip
-pip install -r requirements.txt
-meson setup debug --buildtype=debug --backend=vs
-meson setup release --buildtype=release --backend=vs
-REM Uncomment to build a compilation database for CLion
-REM This requires installing ninja with chocolatey
-REM meson setup _clion_compile_db --backend=ninja
